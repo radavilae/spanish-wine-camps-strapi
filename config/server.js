38d1b0afb3d6ccca7c2cac7cbe52f8e12e22bd9c
radavilae <raul.davila.esp@gmail.com>
2026-03-23 20:57:38 +0100

    Fix: binding port 10000 and host 0.0.0.0 for Render

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), // <-- Asegúrate que sea 0.0.0.0
+  port: env.int('PORT', 10000), // <-- Forzamos el 10000 que pide Render
   app: {
     keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2']),
   },
